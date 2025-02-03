ba820a427afb88978e141336407bada6b6a452bd
Sanskriti Dabhade <sanskritidabhade@gmail.com>
2025-02-03 17:04:29 -0500

    Completed documentation

--- src/Q1.java
+++ src/Q1.java
@@ -1,6 +1,11 @@
 // Name: Sanskriti Avinash Dabhade
-// Student ID: 1225131 | Assignment 02
-// Code Structure:
+// Student ID: 1225131 | Assignment 02 | Question 01
+/* Code Structure: In this program, the user is first asked for an end value N (range 1 - N).
+An if-statement validates whether the input is a positive integer or not. Further, a counter variable is
+initialised to 0 and a do-while loop is implemented. do-while loop runs until the end value is divided to get
+a 1 at the end. The counter keeps incrementing while the condition is true. Out of the loop, counter is incremented
+one more time for the last division. The program exits after printing the total guesses it would take (counter).
+*/
 
 import java.util.Scanner;
 
@@ -12,11 +17,11 @@ public class Q1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a positive end value for the range: ");
         endVal = sc.nextInt();
-        sc.close();
+        sc.close(); // to prevent any leakage.
 
         System.out.println();
 
-        // Checking input: should be a positive integer
+        // Checking input: should be a positive integer.
         if (endVal <= 0) {
             System.out.println("Please enter a positive integer.");
             return;
@@ -25,11 +30,11 @@ public class Q1 {
         // Number of guesses = Divide the number by 2 until 1 is reached + 1.
         int counter = 0;
         do{
-            endVal /= 2;
+            endVal /= 2; // because this value needs to keep updating.
             counter++;
         } while (endVal > 1);
 
-        counter++;
+        counter++; // for the final division when quotient = 0 at 1/2.
 
         // Print the total number of guesses it would take.
         System.out.printf("Total number of guesses it would take: %d", counter);
